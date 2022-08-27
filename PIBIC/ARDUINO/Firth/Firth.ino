c5a6ca9b53323d70dcf8a03996a4cb99ba02fd2f
vnsantoxs <viniciusmelchior.vmls@gmail.com>
2022-08-27 14:31:53 -0300

    new file:   PIBIC/ARDUINO/Firth/Firth.ino 	modified:   "Introdu\303\247\303\243o \303\240 Programa\303\247\303\243o 2/Atividades Extras" (untracked content) 	modified:   Sistemas Operacionais/Atividade pratica(escalonamento e threds) (modified content, untracked content)

---
+++ PIBIC/ARDUINO/Firth/Firth.ino
@@ -0,0 +1,12 @@
+void setup()
+{
+  pinMode(13, OUTPUT);//definindo a porta 13 com saida
+}
+
+void loop() 
+{
+  digitalWrite(13, HIGH);//porta esta fornecendo dados altos(ligado)
+  delay(2000);//inserindo pausa de 2 seg
+  digitalWrite(13, LOW);//porta esta fornecendo dados baixo(desligado)
+  delay(2000);//inserindo pausa de 2 seg
+}
